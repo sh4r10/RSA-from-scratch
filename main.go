959918dc6e39dd4318a9cd17d1341310bfc58966
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-10-23 20:27:23 +0200

    allow encryption and decryption of plain text, implement PKCS#1 padding

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"encoding/json"
 	"flag"
@@ -45,21 +46,83 @@ func main() {
 	}
 }
 
+func addPKCSPadding(messageBytes []byte, blockSize int) []byte {
+	messageLength := len(messageBytes)
+	if messageLength > blockSize-11 {
+		panic("Message is too long for RSA encryption")
+	}
+	paddingLength := blockSize - messageLength - 3
+	padding := make([]byte, paddingLength)
+	_, err := rand.Read(padding)
+	if err != nil {
+		panic("Something went wrong with the padding generation")
+	}
+
+	for i := range padding {
+		for padding[i] == 0 {
+			randValue, err := rand.Int(rand.Reader, big.NewInt(255))
+			if err != nil {
+				panic("Something went wrong with the random number generation")
+			}
+			value := byte(randValue.Uint64())
+			if value != 0 {
+				padding[i] = value
+			}
+		}
+	}
+
+	header := []byte{0x00, 0x02}
+	PS := append(padding, 0x00)
+
+	paddedMessage := append(header, append(PS, messageBytes...)...)
+
+	return paddedMessage
+}
+
+func removePKCSPadding(cipherBytes []byte) []byte {
+	if cipherBytes[0] != 0x02 {
+		panic("Incorrect formatting of encrypted string")
+	}
+
+	i := 2
+	cipherLength := len(cipherBytes)
+	for i < cipherLength {
+		if cipherBytes[i] == 0x00 {
+			i++
+			break
+		}
+		i++
+	}
+
+	if i == cipherLength {
+		panic("No message string")
+	}
+
+	return cipherBytes[i:]
+}
+
 func encryptText(filepath string) {
 	key := readKeyFromFile(filepath).PublicKey
-	var messageText string
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter message to encrypt (no spaces allowed): ")
-	_, err := fmt.Scan(&messageText)
+	fmt.Print("Enter the message to encrypt: ")
+	// Read the input line
+	messageText, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		os.Exit(1)
 	}
 
-	message, bool := new(big.Int).SetString(messageText, 10)
-	if !bool {
-		panic("An error occured reading your message")
-	}
+	// Remove the newline character
+	messageText = messageText[:len(messageText)-1] // Remove t
+
+	// Get the modulus size in bytes
+	modulusBytes := (key.Modulus.BitLen() + 7) / 8
+
+	messageBytes := []byte(messageText)
+	paddedMessage := addPKCSPadding(messageBytes, modulusBytes)
+
+	message := new(big.Int).SetBytes(paddedMessage)
 
 	cipher := new(big.Int).Exp(message, key.E, key.Modulus)
 	fmt.Printf("Your Cipher Text:\n\n%d\n\n", cipher)
@@ -67,22 +130,28 @@ func encryptText(filepath string) {
 
 func decryptText(filepath string) {
 	key := readKeyFromFile(filepath).PrivateKey
-	var cipherText string
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter message to decrypt (no spaces allowed): ")
-	_, err := fmt.Scan(&cipherText)
+	fmt.Print("Enter the cipher text to decrypt: ")
+	cipherText, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		os.Exit(1)
 	}
 
-	cipher, bool := new(big.Int).SetString(cipherText, 10)
-	if !bool {
-		panic("An error occured reading your message")
+	cipherText = cipherText[:len(cipherText)-1] // Remove trailing newline
+	cipher, success := new(big.Int).SetString(cipherText, 10)
+	if !success {
+		panic("Failed to convert the cipher text to a big integer")
 	}
 
+	// Decrypt using RSA
 	message := new(big.Int).Exp(cipher, key.D, key.Modulus)
-	fmt.Printf("\n\nYour Message Text:\n\n%d\n\n", message)
+	messageBytes := message.Bytes()
+
+	// Remove the padding
+	removePadding := removePKCSPadding(messageBytes)
+	fmt.Printf("\n\nYour Message Text:\n\n%s\n\n", string(removePadding))
 }
 
 func generateKey() {
@@ -106,7 +175,7 @@ func generateKey() {
 }
 
 func createNewKeyPair() KeyPair {
-	BITS := 512
+	BITS := 2048
 	var p, q, N *big.Int
 	var errP, errQ error
 
